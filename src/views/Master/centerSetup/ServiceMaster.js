60fb74ceec1f63ab1665e5c2e32d0e192bd30411
[shaikhmd007] <[shaikhmohammad520@gmail.com]>
2022-10-11 18:08:26 +0530

    first commit

--- src/views/Master/centerSetup/ServiceMaster.js
+++ src/views/Master/centerSetup/ServiceMaster.js
@@ -240,7 +240,7 @@ const ServiceMaster = () => {
                     {action &&
                         <div>
                             <CRow className='mt-3'>
-                                <CCol>
+                                <CCol lg={6} md={6} sm={12}>
                                     <CFormInput
                                         className="mb-1"
                                         type="text"
@@ -250,63 +250,8 @@ const ServiceMaster = () => {
                                         onChange={(e) => setServiceName(e.target.value)}
                                         placeholder="Enter Service Name"
                                     />
-                                    <CInputGroup
-                                        value={duration}
-                                        onChange={(e) => setDuration(day + month + select)}
-                                    >
-                                        <CInputGroupText
-                                            component="label"
-                                            htmlFor="inputGroupSelect01"
-                                        >
-                                            Duration
-                                        </CInputGroupText>
-                                        <CFormSelect id="day"
-                                            value={day}
-                                            onChange={durationSet}>
-                                            <option value="">Select</option>
-                                            <option value='1 Day per week'>1 Day per week</option>
-                                            <option value='2 Day per week'>2 Day per week</option>
-                                            <option value='3 Day per week'>3 Day per week</option>
-                                            <option value='4 Day per week'>4 Day per week</option>
-                                            <option value='5 Day per week'>5 Day per week</option>
-                                            <option value='6 Day per week'>6 Day per week</option>
-                                            <option value='7 Day per week'>7 Day per week</option>
-                                        </CFormSelect>
-                                        <CFormSelect id="month"
-                                            value={month}
-                                            onChange={durationSet}>
-                                            <option value="">Select</option>
-                                            <option value=' 1 '>1</option>
-                                            <option value=" 2 ">2</option>
-                                            <option value=" 3 ">3</option>
-                                            <option value=" 4 ">4</option>
-                                            <option value=" 5 ">5</option>
-                                            <option value=" 6 ">6</option>
-                                            <option value=" 7 ">7</option>
-                                            <option value=" 8 ">8</option>
-                                            <option value=" 9 ">9</option>
-                                            <option value=" 10 ">10</option>
-                                            <option value=" 11 ">11</option>
-                                            <option value=" 12 ">12</option>
-                                            <option value=" 13 ">13</option>
-                                            <option value=" 14 ">14</option>
-                                            <option value=" 15 ">15</option>
-                                            <option value=" 16 ">16</option>
-                                            <option value=" 17 ">17</option>
-                                            <option value=" 18 ">18</option>
-                                        </CFormSelect>
-                                        <CFormSelect id="select"
-                                            value={select}
-                                            onChange={durationSet}>
-                                            <option value="">Select</option>
-                                            <option value='Week'>Week</option>
-                                            <option value="Month">Month</option>
-                                            <option value="Year">Year</option>
-                                        </CFormSelect>
-                                    </CInputGroup>
-                                    <CButton className="mt-2" onClick={saveService}>Save</CButton>
                                 </CCol>
-                                <CCol>
+                                <CCol lg={6} md={6} sm={12}>
                                     <CFormInput
                                         className="mb-1"
                                         type="number"
@@ -316,24 +261,8 @@ const ServiceMaster = () => {
                                         onChange={(e) => setFees(e.target.value)}
                                         placeholder="Enter Fees"
                                     />
-                                    <CFormSwitch size="xl" label="Status" style={{ defaultChecked: 'false' }} value={status} onClick={() => setStatus(!status)} />
                                 </CCol>
-                            </CRow>
-                        </div>
-                    }
-                    {action1 &&
-                        <div>
-                            <CRow className='mt-3'>
-                                <CCol>
-                                    <CFormInput
-                                        className="mb-1"
-                                        type="text"
-                                        id="exampleFormControlInput1"
-                                        label="Sub Service Name"
-                                        value={sub_Service_Name}
-                                        onChange={(e) => setSub_Service_Name(e.target.value)}
-                                        placeholder="Enter Sub Service Name"
-                                    />
+                                <CCol lg={6} md={6} sm={12} className="mt-2">
                                     <CInputGroup
                                         value={duration}
                                         onChange={(e) => setDuration(day + month + select)}
@@ -388,9 +317,31 @@ const ServiceMaster = () => {
                                             <option value="Year">Year</option>
                                         </CFormSelect>
                                     </CInputGroup>
-                                    <CButton className="mt-2" onClick={saveSubservice}>Save</CButton>
                                 </CCol>
-                                <CCol>
+                                <CCol lg={6} md={6} sm={12}>
+                                    <CFormSwitch size="xl" label="Status" style={{ defaultChecked: 'false' }} value={status} onClick={() => setStatus(!status)} />
+                                </CCol>
+                                <CCol lg={6} md={6} sm={12}>
+                                    <CButton className="mt-2" onClick={saveService}>Save</CButton>
+                                </CCol>
+                            </CRow>
+                        </div>
+                    }
+                    {action1 &&
+                        <div>
+                            <CRow className='mt-3'>
+                                <CCol lg={6} md={6} sm={12}>
+                                    <CFormInput
+                                        className="mb-1"
+                                        type="text"
+                                        id="exampleFormControlInput1"
+                                        label="Sub Service Name"
+                                        value={sub_Service_Name}
+                                        onChange={(e) => setSub_Service_Name(e.target.value)}
+                                        placeholder="Enter Sub Service Name"
+                                    />
+                                </CCol>
+                                <CCol lg={6} md={6} sm={12}>
                                     <CFormSelect
                                         className="mb-1"
                                         aria-label="Select Service"
@@ -401,8 +352,66 @@ const ServiceMaster = () => {
                                             <option key={index} value={item.ServiceName}>{item.ServiceName}</option>
                                         ))}
                                     </CFormSelect>
+                                </CCol>
+                                <CCol lg={6} md={6} sm={12} className="mt-2">
+                                    <CInputGroup
+                                        value={duration}
+                                        onChange={(e) => setDuration(day + month + select)}
+                                    >
+                                        <CInputGroupText
+                                            component="label"
+                                            htmlFor="inputGroupSelect01"
+                                        >
+                                            Duration
+                                        </CInputGroupText>
+                                        <CFormSelect id="day"
+                                            value={day}
+                                            onChange={durationSet}>
+                                            <option value="">Select</option>
+                                            <option value='1 Day per week'>1 Day per week</option>
+                                            <option value='2 Day per week'>2 Day per week</option>
+                                            <option value='3 Day per week'>3 Day per week</option>
+                                            <option value='4 Day per week'>4 Day per week</option>
+                                            <option value='5 Day per week'>5 Day per week</option>
+                                            <option value='6 Day per week'>6 Day per week</option>
+                                            <option value='7 Day per week'>7 Day per week</option>
+                                        </CFormSelect>
+                                        <CFormSelect id="month"
+                                            value={month}
+                                            onChange={durationSet}>
+                                            <option value="">Select</option>
+                                            <option value=' 1 '>1</option>
+                                            <option value=" 2 ">2</option>
+                                            <option value=" 3 ">3</option>
+                                            <option value=" 4 ">4</option>
+                                            <option value=" 5 ">5</option>
+                                            <option value=" 6 ">6</option>
+                                            <option value=" 7 ">7</option>
+                                            <option value=" 8 ">8</option>
+                                            <option value=" 9 ">9</option>
+                                            <option value=" 10 ">10</option>
+                                            <option value=" 11 ">11</option>
+                                            <option value=" 12 ">12</option>
+                                            <option value=" 13 ">13</option>
+                                            <option value=" 14 ">14</option>
+                                            <option value=" 15 ">15</option>
+                                            <option value=" 16 ">16</option>
+                                            <option value=" 17 ">17</option>
+                                            <option value=" 18 ">18</option>
+                                        </CFormSelect>
+                                        <CFormSelect id="select"
+                                            value={select}
+                                            onChange={durationSet}>
+                                            <option value="">Select</option>
+                                            <option value='Week'>Week</option>
+                                            <option value="Month">Month</option>
+                                            <option value="Year">Year</option>
+                                        </CFormSelect>
+                                    </CInputGroup>
+                                </CCol>
+                                <CCol className="mt-2" lg={6} md={6} sm={12}>
                                     <CRow>
-                                        <CCol>
+                                        <CCol >
 
                                             <CInputGroup>
                                                 <CInputGroupText
@@ -429,6 +438,11 @@ const ServiceMaster = () => {
                                         </CCol>
                                     </CRow>
                                 </CCol>
+                                <CCol lg={6} md={6} sm={12}>
+                                    <CButton className="mt-2" onClick={saveSubservice}>Save</CButton>
+                                </CCol>
+
+
                             </CRow>
                         </div>
                     }
